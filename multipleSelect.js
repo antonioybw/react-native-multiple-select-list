fdae9d9873e19050408c36e4bc02b8501af2e15b
Bowen Yang <antonioyangbowen@gmail.com>
2018-03-15 18:18:09 -0700

    changed textinput style

--- multipleSelect.js
+++ multipleSelect.js
@@ -109,11 +109,8 @@ export default class CustomMultiPicker extends Component {
           </View>
           <TextInput
             style={{
-              width: this.state.pageWidth-20,
               height: 35,
-              margin: 0,
-              marginTop: 10,
-              marginLeft: -25,
+              margin: 10,
               padding: 5,
               paddingLeft: 30,
               borderColor: this.props.iconColor,
